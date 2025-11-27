8d261fb6c424aa7c111b2ff1ba83c14077670c64
RoekoeSR <131486642+RoekoeSR@users.noreply.github.com>
2025-11-27 20:43:24 +0100

    Create train_yolo.py

---
+++ vision/train_yolo.py
@@ -0,0 +1,16 @@
+from ultralytics import YOLO
+
+# load YOLOv8 nano model
+model = YOLO("yolov8n.pt")
+
+# train
+if __name__ == "__main__":
+    model = YOLO("yolov8n.pt")
+    results = model.train(
+        data="C:\\Users\\Riko\\Downloads\\GSD\\GSD-YOLO\\data.yaml",
+        epochs=100,
+        imgsz=640,
+        batch=8,
+        device=0,
+        patience=15
+    )
